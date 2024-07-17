c88e20f7dadd80ec7467924949631a794ab60e52
ArynaPatlakh <patlakhap@gmail.com>
2024-07-17 17:41:13 +0100

    Done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,14 @@
 // functions for HTTP
 import axios from 'axios';
 
-export async function getImages(value) {
-  axios.defaults.baseURL = 'https://pixabay.com/api/';
-  const myKey = '44735338-079c6790302f7dc185545e42d';
+export async function getImages(param) {
+  param.key = '44735338-079c6790302f7dc185545e42d';
   try {
     const response = await axios.get('https://pixabay.com/api/', {
-      params: {
-        key: myKey,
-        q: value,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: 1,
-        per_page: 15,
-      },
+      params: param,
     });
-      return response.data;
+    return response.data;
   } catch (err) {
     err => `Error in FETCH: ${err}`;
   }
-};
+}
--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 //! All logic for Web-Site
 
 import { getImages } from './js/pixabay-api';
-import { cardTemplate } from './js/render-functions';
 import { cardsTemplate } from './js/render-functions';
 
-//* Library
+//*======================================= Library
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import cssLoader from 'css-loader';
-import axios from 'axios';
+
+//*======================================= ELEMENTS
 
 const refs = {
   form: document.querySelector('.search-form'),
@@ -21,26 +22,44 @@ const refs = {
   preLoader: document.querySelector('.pre-loader'),
 };
 
+//*======================================= ADDITIONAL
+
 const lightbox = new SimpleLightbox('.gallery a ', {
   captionDelay: 250,
   captionsData: 'alt',
 });
+
+const param = {
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  page: 1,
+  per_page: 15,
+  maxPage: 0,
+};
+
+//!======================================= SUBMIT ================
 
 refs.form.addEventListener('submit', btnEvent);
 
 async function btnEvent(e) {
   e.preventDefault();
   refs.list.innerHTML = '';
+  refs.btnMore.classList.add('hidden');
+  param.page = 1;
   const userValue = refs.input.value.trim();
   if (userValue.length !== 0 && userValue !== '') {
     refs.load.classList.remove('hidden');
-    await getImages(userValue)
+
+    param.q = userValue;
+    await getImages(param)
       .then(data => {
-        console.log(data);
         if (data.total === 0) {
           refs.load.classList.add('hidden');
           refs.input.value = '';
           refs.list.textContent = '';
+
           iziToast.show({
             message:
               'Sorry, there are no images matching your search query. Please try again!',
@@ -53,6 +72,8 @@ async function btnEvent(e) {
           const markUp = cardsTemplate(data.hits);
           refs.list.innerHTML = markUp;
 
+          param.maxPage = Math.ceil(data.totalHits / param.per_page);
+
           lightbox.refresh();
 
           refs.input.value = '';
@@ -63,4 +84,47 @@ async function btnEvent(e) {
       })
       .catch(err => console.error(err));
   }
+}
+
+//!======================================= LOAD-MORE ==========================
+
+refs.btnMore.addEventListener('click', toDoLoadMore);
+
+async function toDoLoadMore() {
+  param.page += 1;
+
+  refs.preLoader.classList.remove('hidden');
+  refs.btnMore.disabled = true;
+
+  await getImages(param)
+    .then(data => {
+      const markUp = cardsTemplate(data.hits);
+      refs.list.insertAdjacentHTML('beforeend', markUp);
+
+      lightbox.refresh();
+
+      if (param.page === param.maxPage) {
+        refs.btnMore.classList.add('hidden');
+
+        iziToast.show({
+          message: `We're sorry, but you've reached the end of search results`,
+          backgroundColor: '#EF4040',
+          messageColor: 'white',
+          position: 'topRight',
+          class: 'error',
+        });
+      } else {
+        refs.btnMore.disabled = false;
+        refs.preLoader.classList.add('hidden');
+      }
+      const card = document.querySelector('.image');
+      let cardSize = card.getBoundingClientRect();
+      const cardHeight = cardSize.height;
+
+      window.scrollBy({
+        top: cardHeight * 2.5,
+        behavior: 'smooth',
+      });
+    })
+    .catch(err => console.error(err));
 }
